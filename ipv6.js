9d352e6811731ff50091953531558d71d8115abe
538701 <santosh.balasubramanyam@cognizant.com>
2020-07-27 16:58:12 +0000

    Lab 3

--- ipv6.js
+++ ipv6.js
@@ -1,9 +1,3 @@
 
 let mappedAddress = getIpv4MappedIpv6Address(sampleIpv4s[i]);
 module.exports.getIpv4MappedIpv6Address = getIpv4MappedIpv6Address;
-if( mappedAddress ) {
-      console.log(`  IPv4 ${sampleIpv4s[i]} mapped to IPv6 Address: ${mappedAddress}`);
-    } else {
-      console.error(`  Problem converting IPv4 ${sampleIpv4s[i]} into a mapped IPv6 address.`);
-    }
-  
