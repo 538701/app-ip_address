6c3a3d55e305551f5212df38c7b721fdcb8e18f5
538701 <santosh.balasubramanyam@cognizant.com>
2020-08-14 18:41:29 +0000

    Lab 1

--- ipv6.js
+++
@@ -1,5 +0,0 @@
-function main() {
-let mappedAddress = getIpv4MappedIpv6Address(sampleIpv4s[i]);
-module.exports.getIpv4MappedIpv6Address = getIpv4MappedIpv6Address;
-}
-module.exports.getIpv4MappedIpv6Address = getIpv4MappedIpv6Address;
